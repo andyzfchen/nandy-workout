0ad1480cf5e5a342c85c31f6ebf63121fb2a540c
Ruinan <nancywng@sina.com>
2020-04-10 23:17:10 -0400

    workout with preview. Countdown bug

--- abs_mobile/screens/workout_screen.js
+++ abs_mobile/screens/workout_screen.js
@@ -7,40 +7,38 @@ export default class WorkoutScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-		workout: "",
-		time: 0,
+		workout: "Get Ready!",
+		time: 3,
+		counter: 1
     };
   }
-	componentWillMount() {
-		this.exercise_init()
+
+  	preview_exercise() {
+		const workout_data = require('../resources/workouts.json');
+		var random_num = Math.floor(Math.random() * 38);
+		var workout_text = "Up Next: " + workout_data[random_num].name
+		this.setState({
+			workout: workout_text,
+			time: 3,
+			counter: this.state.counter + 1
+        })
 	}
 
-	exercise_init() {
+    start_exercise() {
 		const workout_data = require('../resources/workouts.json');
 		var random_num = Math.floor(Math.random() * 38);
 		var workout_text = workout_data[random_num].name
 		this.setState({
 			workout: workout_text,
-			time: 4
-		})
-	}
-
-    workout_selector() {
-		this.setState({
-			time: 5
-		})
-		const workout_data = require('../resources/workouts.json');
-		var random_num = Math.floor(Math.random() * 38);
-		var workout_text = workout_data[random_num].name
-		this.setState({
-			workout: workout_text,
-			time: 0
+			time: 5,
+			counter: this.state.counter + 1
         })
     }
 
   render() {
     return (
       <View style={styles.container}>
+		<Text style={styles.workout_text}> {this.state.counter} </Text>
         <Text style={styles.workout_text}> {this.state.workout} </Text>
         <CountDown
             until={this.state.time + 1}
@@ -48,7 +46,12 @@ export default class WorkoutScreen extends Component {
             timeLabels={{s: null}}
             digitStyle={{backgroundColor: THEME_BLUE}}
 			digitTxtStyle={{color: '#FFF'}}
-			onFinish={() => this.workout_selector()}
+			onFinish={() => {
+				if(this.state.counter%2 == 0) {
+					this.preview_exercise()
+				} else {
+					this.start_exercise()
+				}}}
             // onFinish={() => alert('Congradulations on finishing the workout!')}
             size={60}
       />
