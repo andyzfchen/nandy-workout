686ea7519de3fcafdec6f0fd48cce86a326b3150
Andy Chen <andych@umich.edu>
2020-04-10 23:29:34 -0700

    Finished translating bash to python.

--- python/do_abs.py
+++ python/do_abs.py
@@ -32,7 +32,7 @@ def ex_countdown (t_event):
 # declare next exercise
 def ex_next (ex_name):
   play_mp3("next exercise")
-  time.sleep(1)
+  time.sleep(2)
   play_mp3(ex_name)
   return
 
@@ -52,33 +52,65 @@ def breaktime (t_event=20):
 ###################################
 
 parser = argparse.ArgumentParser(description="Process exercise configuration.")
-parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                   help='an integer for the accumulator')
-parser.add_argument('--sum', dest='accumulate', action='store_const',
-                   const=sum, default=max,
-                   help='sum the integers (default: find the max)')
+parser.add_argument('--mode',type=int, default=0, help="Workout mode: 0 - easy; 1 - hard.")
+parser.add_argument('--time',type=int, default=6, help="Workout duration in minutes.")
 
 args = parser.parse_args()
-print(args.accumulate(args.integers))
-
-exercises = np.genfromtxt('../data/abs_exercises.csv', dtype=str, delimiter=',')
-print(exercises)
-print(np.shape(exercises))
-
 
 # Starting sequence
 print("App is starting.")
-print("Duration: $time mins")
-print("Hardcore: $hard")
+play_mp3("starting abdominal exercises")
+time.sleep(3)
 
-say "Starting abdominal exercises."
-say "$time minutes."
-if $hard
-then
-  say "Hard mode."
-else
-  say "Easy mode."
-fi
-sleep 2
-echo
+print("Duration: %d mins" % args.time)
+play_mp3("%d minutes" % args.time)
+time.sleep(2)
 
+if (args.mode==0):
+  print("Difficulty: easy")
+  play_mp3("easy mode")
+elif (args.mode==1):
+  print("Difficulty: hard")
+  play_mp3("hard mode")
+time.sleep(2)
+
+
+# Loads exercises
+exercises = np.genfromtxt('../data/abs_exercises.csv', dtype=str, delimiter=',')
+N_tot = np.shape(exercises)[0]
+print("Total possible exercises: %d" % N_tot)
+
+np.random.shuffle(exercises)
+
+N_ex = args.time*2
+print("Total session exercises: %d" % N_ex)
+
+# Workout sequence
+for j in range(N_ex):
+  if ((j%6)==5):    # breaktime
+    play_mp3("break time")
+    breaktime()
+
+    if ((j+1)!=N_ex):
+      print("Next exercise: %s" % exercises[j+1])
+      ex_next(exercises[j])
+
+    ex_countdown(10)
+  else              # exercise:
+    print("Current exercise: %s" % exercises[j])
+    ex_start(exercises[j])
+    exercise()
+
+    if ((j+1)==N_ex):
+      print("almost done!")
+    if (((j%6)+1)==5):
+      print("Next exercise: break time")
+      ex_next("break time")
+    else:
+      print("Next exercise: %s" % exercises[j+1]) 
+      ex_next(exercises[j])
+
+    ex_countdown(10)
+
+print("Exercise complete.")
+play_mp3("congratulations abdominal exercises completed")
